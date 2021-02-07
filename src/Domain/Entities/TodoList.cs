4572b6c013736803b9e8128c0f817d71b93c0004
Halim <46631802+HalimHamidov@users.noreply.github.com>
2021-02-07 22:01:12 +0300

    Initialize dotnet5

---
+++ src/Domain/Entities/TodoList.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Domain.Common;
+using System.Collections.Generic;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class TodoList : AuditableEntity
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Colour { get; set; }
+
+        public IList<TodoItem> Items { get; private set; } = new List<TodoItem>();
+    }
+}
---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,50 @@
+﻿using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Infrastructure.Files;
+using CleanArchitecture.Infrastructure.Identity;
+using CleanArchitecture.Infrastructure.Persistence;
+using CleanArchitecture.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseInMemoryDatabase("CleanArchitectureDb"));
+            }
+            else
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("DefaultConnection"),
+                        b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            }
+
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+
+            services.AddScoped<IDomainEventService, DomainEventService>();
+
+            services.AddDefaultIdentity<ApplicationUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+            services.AddTransient<IDateTime, DateTimeService>();
+            services.AddTransient<IIdentityService, IdentityService>();
+            services.AddTransient<ICsvFileBuilder, CsvFileBuilder>();
+
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
+
+            return services;
+        }
+    }
+}
